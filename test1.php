8a72e45f3b67645289a7d78f04cef4d77f14fcff
stevenchen5899 <t0938139311@gmail.com>
2018-11-19 12:04:20 +0800

    Update test1.php

--- test1.php
+++ test1.php
@@ -3,5 +3,5 @@ $json_str=file_get_contents('php://input');//接收request的body
 $json_obj=json_decode($json_str);//轉成json格式
 
 $myfile=fopen("log.txt","w+") or die("unable to open file!");//設定一個log.txt來印訊息
-fwrite(＄smyfile, "\xEF\xBB\xBF".$json_str);//在字串上面加上\xef\xbb\xbf 轉成utf8格式
-？>
+fwrite($myfile, "\xEF\xBB\xBF".$json_str);//在字串上面加上\xef\xbb\xbf 轉成utf8格式
+?>
