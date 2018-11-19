982220cdf3f5f4c3b06532852acb22d7bd3eed3d
stevenchen5899 <t0938139311@gmail.com>
2018-11-19 12:22:57 +0800

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,7 +1,32 @@
 <?php
-$json_str=file_get_contents('php://input');//接收request的body
-$json_obj=json_decode($json_str);//轉成json格式
+  $json_str = file_get_contents('php://input'); //接收request的body
+  $json_obj = json_decode($json_str); //轉成json格式
+  
+  $myfile = fopen("log.txt", "w+") or die("Unable to open file!"); //設定一個log.txt來印訊息
+  fwrite($myfile, "\xEF\xBB\xBF".$json_str); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
+  
+  $sender_userid = $json_obj->events[0]->source->userId; //取得訊息發送者的id
+  $sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
+  
+  $response = array (
+    "to" => $sender_userid,
+    "messages" => array (
+      array (
+        "type" => "text",
+        "text" => "Hello. You say". $sender_txt
+      )
+    )
+  );
+  
+ fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
 
-$myfile=fopen("log.txt","w+") or die("unable to open file!");//設定一個log.txt來印訊息
-fwrite($myfile, "\xEF\xBB\xBF".$json_str);//在字串上面加上\xef\xbb\xbf 轉成utf8格式
+  $header[] = "Content-Type: application/json";
+  $header[] = "Authorization: Bearer iewVIXyfpmUm9qSF230NhA9kajJ5b5kSHVUL669amxxIiJh9xTgIibV5suZ3ARtgp3PglEUjMQhyC6bvADvqLAF5W9PoKC2qh4rkamH4o1/ysN0K/gChyC0bt3w+DVXqqee2Fzj5IIK3vE9egpv8cwdB04t89/1O/w1cDnyilFU=";
+  $ch = curl_init("https://api.line.me/v2/bot/message/push");
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
+  $result = curl_exec($ch);
+  curl_close($ch);
 ?>
